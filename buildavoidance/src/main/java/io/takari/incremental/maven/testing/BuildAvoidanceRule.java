a427a6ca84fc4801691c0a1ad9e5e09aeebd9a23
Igor Fedorenko <igor@ifedorenko.com>
2014-01-27 12:09:37 -0500

    added buildavoidance maven test helpers
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- buildavoidance/src/main/java/io/takari/incremental/maven/testing/BuildAvoidanceRule.java
+++ buildavoidance/src/main/java/io/takari/incremental/maven/testing/BuildAvoidanceRule.java
@@ -1,6 +1,12 @@
 package io.takari.incremental.maven.testing;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.MojoExecutionEvent;
@@ -10,6 +16,7 @@ import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.project.MavenProject;
+import org.junit.Assert;
 
 public class BuildAvoidanceRule extends MojoRule {
 
@@ -19,6 +26,8 @@ public class BuildAvoidanceRule extends MojoRule {
 
   public void executeMojo(MavenSession session, MavenProject project, MojoExecution execution)
       throws Exception {
+    getBuildContextLog().clear();
+
     MojoExecutionScope scope = lookup(MojoExecutionScope.class);
     try {
       scope.enter();
@@ -50,5 +59,61 @@ public class BuildAvoidanceRule extends MojoRule {
 
   public BuildContextLog getBuildContextLog() throws Exception {
     return lookup(BuildContextLog.class);
+  }
+
+  /**
+   * Asserts specified paths were output during the build
+   */
+  public void assertBuildOutputs(File basedir, String... paths) throws Exception {
+    Set<File> expected = toFileSet(basedir, paths);
+    Set<File> actual = new TreeSet<File>(getBuildContextLog().getRegisteredOutputs());
+    Assert.assertEquals(toString(expected), toString(actual, true));
+  }
+
+  /**
+   * Asserts specified paths were deleted during the build
+   */
+  public void assertDeletedOutputs(File basedir, String... paths) throws Exception {
+    Set<File> expected = toFileSet(basedir, paths);
+    Set<File> actual = new TreeSet<File>(getBuildContextLog().getDeletedOutputs());
+    Assert.assertEquals(toString(expected), toString(actual, false));
+  }
+
+  /**
+   * Asserts messages were associated with the specified path during the build.
+   */
+  public void assertMessages(File basedir, String path, String... messages) throws Exception {
+    List<String> actual =
+        new ArrayList<String>(getBuildContextLog().getMessages(new File(basedir, path)));
+    List<String> expected = Arrays.asList(messages);
+    Assert.assertEquals(toString(expected), toString(actual));
+  }
+
+  private static String toString(Collection<?> objects) {
+    StringBuilder sb = new StringBuilder();
+    for (Object file : objects) {
+      sb.append(file.toString()).append('\n');
+    }
+    return sb.toString();
+  }
+
+  private static String toString(Collection<File> files, boolean canRead) {
+    StringBuilder sb = new StringBuilder();
+    for (File file : files) {
+      if (canRead != file.canRead()) {
+        sb.append(file.canRead() ? "EXISTS " : "DOES NOT EXIST ");
+      }
+      sb.append(file.toString()).append('\n');
+    }
+    return sb.toString();
+  }
+
+
+  private Set<File> toFileSet(File basedir, String... paths) {
+    Set<File> expected = new TreeSet<File>();
+    for (String path : paths) {
+      expected.add(new File(basedir, path));
+    }
+    return expected;
   }
 }
