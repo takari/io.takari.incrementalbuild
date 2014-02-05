dd5e7aa2244c1776b114b859f1b5d8d00a31a8ce
Igor Fedorenko <igor@ifedorenko.com>
2014-02-05 14:53:48 -0500

    fixed IT failure when pom.xml maven.version property changes
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild-its/src/test/java/io/takari/incrementalbuild/maven/it/BasicTest.java
+++ incrementalbuild-its/src/test/java/io/takari/incrementalbuild/maven/it/BasicTest.java
@@ -2,10 +2,13 @@ package io.takari.incrementalbuild.maven.it;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.plugin.testing.resources.TestResources;
+import org.codehaus.plexus.util.IOUtil;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,8 +17,6 @@ public class BasicTest {
 
   @Rule
   public final TestResources resources = new TestResources("src/test/it", "target/it");
-
-  private final String mavenVersion = "3.2.0-SNAPSHOT";
 
   @Test
   public void testBasic_incrementalBuild() throws Exception {
@@ -32,7 +33,7 @@ public class BasicTest {
   }
 
   protected Verifier getVerifier(File basedir) throws VerificationException, IOException {
-    File mavenHome = new File("target/dependency/apache-maven-" + mavenVersion);
+    File mavenHome = new File("target/dependency/apache-maven-" + getMavenVersion());
     Assert.assertTrue("Can't locate maven home, make sure to run 'mvn generate-test-resources': "
         + mavenHome, mavenHome.isDirectory());
     // XXX somebody needs to fix this in maven-verifier already
@@ -40,4 +41,19 @@ public class BasicTest {
     return new Verifier(basedir.getAbsolutePath());
   }
 
+
+  private static String getMavenVersion() throws IOException {
+    Properties properties = new Properties();
+    InputStream is = BasicTest.class.getClassLoader().getResourceAsStream("test.properties");
+    try {
+      properties.load(is);
+    } finally {
+      IOUtil.close(is);
+    }
+    String version = properties.getProperty("maven.version");
+    if (version == null) {
+      throw new IllegalStateException("Could not determine maven version");
+    }
+    return version;
+  }
 }
