fb34a3599d36997d7e6f9cc96fc5be46f4061043
Igor Fedorenko <igor@ifedorenko.com>
2014-02-25 13:34:29 -0500

    clarified ResourceHolder <R> type parameter assumptions
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild/src/main/java/io/takari/incrementalbuild/spi/ResourceHolder.java
+++ incrementalbuild/src/main/java/io/takari/incrementalbuild/spi/ResourceHolder.java
@@ -7,14 +7,18 @@ import java.io.Serializable;
 /**
  * Wraps input or output resource and logic and state necessary to determine if the resource has or
  * has not changed since the holder instance was created.
- *
+ * 
+ * <p>
+ * Current implementation assumes R is some sort of serializable resource reference that can be used
+ * to locate resource and determine resource up-to-date status against resource state captured in
+ * resource holder instance. For example, to determine if an input has changed, implementation needs
+ * to locate input's included inputs and check their status.
+ * 
  * @param <R> must provide correct implementation of {@code hashCode()} and {@code equals(Object)}
  */
 public interface ResourceHolder<R extends Serializable> extends Serializable {
 
   public R getResource();
-
-  // TODO introduce K getResourceKey(), relax R serializable requirement
 
   public ResourceStatus getStatus();
 }
