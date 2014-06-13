8e84112fba100ca692f31b90dd15bf5c3f35ba2b
Igor Fedorenko <igor@ifedorenko.com>
2014-06-13 09:27:00 -0400

    removed outdated javadoc information
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild/src/main/java/io/takari/incrementalbuild/maven/internal/MavenBuildContext.java
+++ incrementalbuild/src/main/java/io/takari/incrementalbuild/maven/internal/MavenBuildContext.java
@@ -18,21 +18,6 @@ import org.apache.maven.project.MavenProject;
 
 /**
  * Maven specific BuildContext implementation that provides
- * <ul>
- * <li>Conventional location of incremental build state under ${build.build.directory}/incremental.
- * In the future, this may become configurable via well-known project property.</li>
- * <li>Automatic detection of configuration changes based on
- * <ul>
- * <li>Maven plugin artifacts GAVs, file sizes and timestamps</li>
- * <li>Project effective pom.xml. In the future, this may be narrowed down.</li>
- * <li>Maven session execution, i.e. user and system, properties.</li>
- * </ul>
- * </li>
- * </ul>
- * 
- * @TODO decide how to handle volatile properties like ${maven.build.timestamp}. Should we always
- *       ignore them? Are there cases where output has to be always regenerated just to include new
- *       build timestamp, for example?
  */
 @Named
 @Typed({DefaultBuildContext.class, MavenBuildContext.class})
