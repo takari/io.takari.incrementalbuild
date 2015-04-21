60fb5bec9ce217cc6cb4e1d3eb58ff8d979e8fb2
Igor Fedorenko <igor@ifedorenko.com>
2015-04-21 16:33:15 -0400

    openjdk: consistently use File#lastModified
    Fixes #4
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild/src/main/java/io/takari/incrementalbuild/spi/FilesystemWorkspace.java
+++ incrementalbuild/src/main/java/io/takari/incrementalbuild/spi/FilesystemWorkspace.java
@@ -1,7 +1,5 @@
 package io.takari.incrementalbuild.spi;
 
-import io.takari.incrementalbuild.workspace.Workspace;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+
+import io.takari.incrementalbuild.workspace.Workspace;
 
 public class FilesystemWorkspace implements Workspace {
 
@@ -62,10 +62,15 @@ public class FilesystemWorkspace implements Workspace {
     final Path basepath = basedir.toPath();
     Files.walkFileTree(basepath, new SimpleFileVisitor<Path>() {
       @Override
-      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        final long lastModified = attrs.lastModifiedTime().toMillis();
-        final long length = attrs.size();
-        visitor.visit(file.toFile(), lastModified, length, ResourceStatus.NEW);
+      public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+        // BasicFileAttributes#lastModifiedTime() and File#lastModified() appear to have different
+        // resolution in some environments and mixing the two results in "Unexpected input change"
+        // exceptions.
+        // https://github.com/takari/io.takari.incrementalbuild/pull/5
+        final File file = path.toFile();
+        final long lastModified = file.lastModified();
+        final long length = file.length();
+        visitor.visit(file, lastModified, length, ResourceStatus.NEW);
         return FileVisitResult.CONTINUE;
       }
     });
