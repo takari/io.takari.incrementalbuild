b75e595d03dcd460266463a5ee81c2c812001363
Igor Fedorenko <ifedorenko@salesforce.com>
2017-04-23 17:20:18 -0400

    collapsed FileMatcher and AbstractMatcher
    Signed-off-by: Igor Fedorenko <ifedorenko@salesforce.com>

--- takari-builder-common/src/main/java/io/takari/builder/internal/pathmatcher/FileMatcher.java
+++ takari-builder-common/src/main/java/io/takari/builder/internal/pathmatcher/FileMatcher.java
@@ -2,12 +2,74 @@ package io.takari.builder.internal.pathmatcher;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
 
-public class FileMatcher extends AbstractMatcher {
+public class FileMatcher {
+  private static interface Matcher {
+    public boolean matches(String path);
+  }
+
+  private static final Matcher MATCH_EVERYTHING = new Matcher() {
+    @Override
+    public boolean matches(String path) {
+      return true;
+    }
+  };
+
+  private final Matcher includesMatcher;
+  private final Matcher excludesMatcher;
+
+  protected final String basedir;
+
+  private static Matcher fromStrings(String basepath, Collection<String> globs,
+      Matcher everything) {
+    if (globs == null || globs.isEmpty()) {
+      return null; // default behaviour appropriate for includes/excludes pattern
+    }
+    final ArrayList<String> normalized = new ArrayList<>();
+    for (String glob : globs) {
+      if ("*".equals(glob) || "**".equals(glob) || "**/*".equals(glob)) {
+        return everything; // matches everything
+      }
+
+      StringBuilder gb = new StringBuilder();
+      gb.append(basepath).append('/');
+      gb.append(glob.startsWith("/") ? glob.substring(1) : glob);
+
+      // from https://ant.apache.org/manual/dirtasks.html#patterns
+      // There is one "shorthand": if a pattern ends with / or \, then ** is appended
+      if (glob.endsWith("/")) {
+        gb.append("**");
+      }
+      normalized.add(gb.toString().replace('/', File.separatorChar));
+    }
+    final Plexus_MatchPatterns matcher = Plexus_MatchPatterns.from(normalized);
+    return new Matcher() {
+      @Override
+      public boolean matches(String path) {
+        return matcher.matches(path, false);
+      }
+    };
+  }
+
+  public boolean matches(String path) {
+    if (basedir != null && !path.startsWith(basedir)) {
+      return false;
+    }
+    if (excludesMatcher != null && excludesMatcher.matches(path)) {
+      return false;
+    }
+    if (includesMatcher != null) {
+      return includesMatcher.matches(path);
+    }
+    return true;
+  }
 
   private FileMatcher(String basedir, Matcher includesMatcher, Matcher excludesMatcher) {
-    super(basedir, includesMatcher, excludesMatcher);
+    this.basedir = basedir;
+    this.includesMatcher = includesMatcher;
+    this.excludesMatcher = excludesMatcher;
   }
 
   public boolean matches(Path file) {
